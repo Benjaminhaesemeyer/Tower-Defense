231aadfe15714993f8bf2000dff83a907c18b85f
Ben Haesemeyer <Benjaminhaesemeyer@gmail.com>
2017-11-28 10:01:12 -0600

    Enemy movement functions moved to a seperate script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,18 @@
 
 public class Enemy : MonoBehaviour {
 
-	public float speed = 10f;
-	public float health = 100;
-	public int value = 50;
+	public float startSpeed = 10f;
 
+	[HideInInspector]
+	public float speed;
+
+	public float health = 100;
+	public int worth = 50;
 	public GameObject deathEffect;
 
-	private Transform target;
-	private int wavepointIndex = 0;
 
-	void Start () {
-		target = Waypoints.points [0];
+	void Start(){
+		speed = startSpeed;
 	}
 
 	public void TakeDamage(float amount){
@@ -22,39 +23,17 @@ public class Enemy : MonoBehaviour {
 			Die ();
 		}
 	}
+
+	public void Slow (float pct){
+		speed = startSpeed * (1f- pct);
+	}
+
 	void Die() {
-		PlayerStats.Money += value;
+		PlayerStats.Money += worth;
 
 		GameObject effect = (GameObject)Instantiate (deathEffect, transform.position, Quaternion.identity);
 		Destroy (effect, 5f);
 
-		Destroy (gameObject);
-	}
-
-
-	void Update() {
-		Vector3 dir = target.position - transform.position;
-		transform.Translate (dir.normalized * speed * Time.deltaTime, Space.World);
-
-		if (Vector3.Distance (transform.position, target.position) <= 0.4f) {
-			GetNextWaypoint ();
-		}
-	}
-
-	void GetNextWaypoint(){
-
-		if (wavepointIndex >= Waypoints.points.Length - 1) {
-			EndPath ();
-			return;
-
-		}
-
-		wavepointIndex++;
-		target = Waypoints.points [wavepointIndex];
-	}
-
-	void EndPath() {
-		PlayerStats.Lives--;
 		Destroy (gameObject);
 	}
 }
