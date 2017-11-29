ec4e20f710ab91983d583794d5fdd53a1c2c2f56
Ben Haesemeyer <Benjaminhaesemeyer@gmail.com>
2017-11-29 10:56:54 -0600

    Code comments added to: EnemyMovement script

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,42 +5,44 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour {
 
-	private Transform target;
-	private int wavepointIndex = 0;
+	private Transform target; //Setting target to move towards
+    private int waypointIndex = 0; //Starting point for wavepointIndex
 
-	private Enemy enemy;
+	private Enemy enemy; //Enemy variable to refer to
 
 
 	void Start () {
-		enemy = GetComponent<Enemy> ();
+		enemy = GetComponent<Enemy> (); //Access the enemy
 
-		target = Waypoints.points [0];
+		target = Waypoints.points [0]; //First waypoint to target 
 	}
 
 	void Update() {
+        //Vector direction we want enemy to move towards = target position - current position
 		Vector3 dir = target.position - transform.position;
+        //Speed for the enemies transform to move at
 		transform.Translate (dir.normalized * enemy.speed * Time.deltaTime, Space.World);
-
+        //If we reach the current waypoint we are targeting get the next one
 		if (Vector3.Distance (transform.position, target.position) <= 0.4f) {
 			GetNextWaypoint ();
 		}
-		enemy.speed = enemy.startSpeed;
+		enemy.speed = enemy.startSpeed; //resetting the speed to default
 	}
-
+    //Function to assign next waypoint for the enemy to target
 	void GetNextWaypoint(){
-
-		if (wavepointIndex >= Waypoints.points.Length - 1) {
+        //If enemy reaches the last waypoint call endPath()
+		if (waypointIndex >= Waypoints.points.Length - 1) {
 			EndPath ();
 			return;
 
 		}
-
-		wavepointIndex++;
-		target = Waypoints.points [wavepointIndex];
+        //Get the next waypoint in the index
+		waypointIndex++;
+		target = Waypoints.points [waypointIndex];
 	}
-
+    //Runs when enemy reaches end of waypoint path
 	void EndPath() {
-		PlayerStats.Lives--;
-		Destroy (gameObject);
+		PlayerStats.Lives--; //Subtract life from player
+        Destroy (gameObject); //Destroy enemy
 	}
 }
