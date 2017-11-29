772dc06ea1acacf66b47a303e771259ddeb1c587
Ben Haesemeyer <Benjaminhaesemeyer@gmail.com>
2017-11-29 10:27:23 -0600

    Code comments added to: Enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,20 +2,20 @@
 
 public class Enemy : MonoBehaviour {
 
-	public float startSpeed = 10f;
+	public float startSpeed = 10f; //Speed of enemy at begining of the game
 
 	[HideInInspector]
-	public float speed;
+	public float speed; //Speed variable we can assign a value later
 
-	public float health = 100;
-	public int worth = 50;
-	public GameObject deathEffect;
+	public float health = 100; //Default health for enemies
+	public int worth = 50; //Money player recieves for destroying enemy
+	public GameObject deathEffect; //Particle effect when enemy is destroyed 
 
 
 	void Start(){
-		speed = startSpeed;
+		speed = startSpeed; //Setting speed value to 10f at the start
 	}
-
+    //Function to damage enemies health
 	public void TakeDamage(float amount){
 		health -= amount;
 
@@ -23,17 +23,19 @@ public class Enemy : MonoBehaviour {
 			Die ();
 		}
 	}
-
+    //Function to slow down enemies speed 
 	public void Slow (float pct){
-		speed = startSpeed * (1f- pct);
+		speed = startSpeed * (1f- pct); //Slowing down enemies by percent
 	}
-
+    //Function to destroy enemies 
 	void Die() {
-		PlayerStats.Money += worth;
+		PlayerStats.Money += worth; //Add money to the players bank 
 
+        //Run the death particle effect 
 		GameObject effect = (GameObject)Instantiate (deathEffect, transform.position, Quaternion.identity);
 		Destroy (effect, 5f);
 
+        //Remove the enemy object from the hierarchy
 		Destroy (gameObject);
 	}
 }
