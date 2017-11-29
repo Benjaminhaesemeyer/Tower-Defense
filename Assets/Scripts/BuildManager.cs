0a1fc5247a9d09b0f38946637a351fee5700b072
Ben Haesemeyer <Benjaminhaesemeyer@gmail.com>
2017-11-29 12:19:00 -0600

    Node UI toggles on and off

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -16,6 +16,8 @@ public class BuildManager : MonoBehaviour {
 	private TurretBlueprint turretToBuild;
     private Node selectedNode;
 
+    public NodeUI nodeUI;
+
 
 	public bool CanBuild { get{ return turretToBuild != null; }}
 
@@ -41,12 +43,24 @@ public class BuildManager : MonoBehaviour {
 
     public void SelectNode(Node node)
     {
+        if(selectedNode == node)
+        {
+            DeselectNode();
+            return;
+        }
         selectedNode = node;
         turretToBuild = null;
+        nodeUI.SetTarget(node);
     } 
+
+    public void DeselectNode()
+    {
+        selectedNode = null;
+        nodeUI.Hide();  
+    }
 
 	public void SelectTurretToBuild(TurretBlueprint turret){
 		turretToBuild = turret;
-        selectedNode = null;
+        DeselectNode();
 	}
 }
